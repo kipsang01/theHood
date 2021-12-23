da81080b08bce612425d8e51e92d3b88adf7ccf8
kipsang01 <enock.kipsang@student.moringaschool.com>
2021-12-23 11:14:31 +0300

    Add neighborhood model

--- hood/models.py
+++ hood/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 from cloudinary.models import CloudinaryField
 
 
@@ -8,7 +10,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500,blank=True)
     profile_pic = CloudinaryField('image')
-    location = models.CharField(max_length=100,blank=True)
+    neigborhood = models.CharField(max_length=100,blank=True)
     
     def __str__(self):
         return str(self.user)
@@ -21,3 +23,10 @@ class Profile(models.Model):
     @receiver(post_save, sender=User)
     def save_user_profile(sender, instance, **kwargs):
         instance.profile.save()
+
+
+class Neigborhood(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    creator = models.ForeignKey(User)
+    date_created = models.DateTimeField(auto_now_add=True)
